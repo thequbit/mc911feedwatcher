2453bbaed32545661d778d32c8b09fe9501a8e19
thequbit <thequbit@gmail.com>
2013-02-21 23:00:54 -0500

    loads in known four letter agencies

---
+++ database/populate_agencies.sql
@@ -0,0 +1,94 @@
+UPDATE agencies SET longname="Monroe County Police", websiteurl="http://www2.monroecounty.gov/sheriff-index.php" WHERE shortname="MCOP";
+UPDATE agencies SET longname="Rochester City Police", websiteurl="http://www.cityofrochester.gov/police/" WHERE shortname="CTYP";
+UPDATE agencies SET longname="East Rochester Police", websiteurl="http://eastrochester.org/departments/police/police.php"  WHERE shortname="EROP";
+UPDATE agencies SET longname="East Rochester Fire", websiteurl="http://www.erfd.org"  WHERE shortname="EROF";
+UPDATE agencies SET longname="Greece Police", websiteurl="http://greeceny.gov/police/home"  WHERE shortname="GREP";
+UPDATE agencies SET longname="New York State Police", websiteurl="http://www.troopers.ny.gov/"  WHERE shortname="NYSP";
+UPDATE agencies SET longname="Regional Traffic Operations Center", websiteurl="http://www.monroecounty.gov/dot-rtoc.php"  WHERE shortname="RTOC";
+UPDATE agencies SET longname="Rochester City Fire", websiteurl="http://www.cityofrochester.gov/article.aspx?id=8589936630"  WHERE shortname="CTYF";
+UPDATE agencies SET longname="Ogden Police", websiteurl="http://www.ogdencity.com/en/public_safety/microsite_Police.aspx"  WHERE shortname="OGDP";
+UPDATE agencies SET longname="Gates Police", websiteurl="http://www.townofgates.org/index.php?option=com_content&view=category&layout=blog&id=8&Itemid=38" WHERE shortname="GATP";
+UPDATE agencies SET longname="Fairport Police", websiteurl="http://www.fairport.ny.us/POLICE_DEPARTMENT.cfm" WHERE shortname="FAIP";
+UPDATE agencies SET longname="Irondequoit Police", websiteurl="http://www.irondequoit.org/content/view/131/273/" WHERE shortname="IROP";
+UPDATE agencies SET longname="Brighton Fire", websiteurl="http://www.brightonfd.org/" WHERE shortname="BRIF";
+UPDATE agencies SET longname="Brighton Ambulance", websiteurl="http://www.brightonambulance.org/" WHERE shortname="BRIE";
+UPDATE agencies SET longname="Brighton Police", websiteurl="http://www.townofbrighton.org/index.aspx?nid=221" WHERE shortname="BRIP";
+UPDATE agencies SET longname="Hilton Fire", websiteurl="http://www.hiltonfd.org/" WHERE shortname="HILF";
+UPDATE agencies SET longname="Brockport Police", websiteurl="http://www.brockportpolice.org/" WHERE shortname="BROP";
+UPDATE agencies SET longname="Chili Fire", websiteurl="http://www.chilifire.org/home.php" WHERE shortname="CHIF";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="RMEE";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="RROF";
+UPDATE agencies SET longname="Monroe Ambulance", websiteurl="http://www.monroeambulance.com/" WHERE shortname="MONE";
+UPDATE agencies SET longname="Gates Ambulance", websiteurl="http://www.gatesems.org/" WHERE shortname="GATE";
+UPDATE agencies SET longname="Gates Fire", websiteurl="http://www.gatesfd.org/" WHERE shortname="GATF";
+UPDATE agencies SET longname="Webster Police", websiteurl="http://ci.webster.ny.us/index.aspx?nid=86" WHERE shortname="WEBP";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="BBAF";
+UPDATE agencies SET longname="Churchville Fire", websiteurl="http://www.churchvillefire.com/" WHERE shortname="CHUF";
+UPDATE agencies SET longname="Henrietta Fire", websiteurl="http://www.henriettafire.com/" WHERE shortname="HENF";
+UPDATE agencies SET longname="Fairport Fire", websiteurl="http://www.fairportfd.org/" WHERE shortname="FAIF";
+UPDATE agencies SET longname="Chili Ambulance", websiteurl="http://chiliambulance.org/" WHERE shortname="CHIE";
+UPDATE agencies SET longname="Spencerport Ambulance", websiteurl="http://www.spencerportambulance.org/" WHERE shortname="SPEE";
+UPDATE agencies SET longname="Spencerport Fire", websiteurl="http://www.spencerportfire.org/news/index/layoutfile/home" WHERE shortname="SPEF";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="PPLF";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="RCUF";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="NGRF";
+UPDATE agencies SET longname="Penfield Fire", websiteurl="http://www.penfieldfire.org/" WHERE shortname="PENF";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="OTHF";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="SPAF";
+UPDATE agencies SET longname="Union Hill Association Fire", websiteurl="http://www.uhfd.org" WHERE shortname="UHIF";
+UPDATE agencies SET longname="Brockport Fire", websiteurl="http://brockportfire.org/" WHERE shortname="BROF";
+UPDATE agencies SET longname="Webster Fire", websiteurl="http://www.websterfire.com" WHERE shortname="WEBF";
+UPDATE agencies SET longname="Henrietta Ambulance", websiteurl="http://henriettaambulance.org/" WHERE shortname="HENE";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="BARF";
+UPDATE agencies SET longname="Churchville Ambulance", websiteurl="http://www.churchvillefire.org/" WHERE shortname="CHUE";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="HSOP";
+UPDATE agencies SET longname="Pittsford Ambulance", websiteurl="http://www.pittsfordambulance.org/" WHERE shortname="PITE";
+
+UPDATE agencies SET longname="Egypt Fire", websiteurl="http://www.egyptfd.org/" WHERE shortname="EGYF";
+
+UPDATE agencies SET longname="West Webster Fire", websiteurl="http://westwebsterfd.org/" WHERE shortname="WWBF";
+
+UPDATE agencies SET longname="Rush Fire", websiteurl="http://www.rushfd.org/" WHERE shortname="RUSF";
+UPDATE agencies SET longname="Pittsford Fire", websiteurl="http://www.pittsfordfd.org/" WHERE shortname="PITF";
+UPDATE agencies SET longname="Mendon Fire", websiteurl="http://www.mendonfd.us/" WHERE shortname="MENF";
+UPDATE agencies SET longname="Hamlin Fire", websiteurl="http://www.hamlinfire.com" WHERE shortname="HAMF";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="LAUF";
+UPDATE agencies SET longname="Hamlin Ambulance", websiteurl="http://www.hamlinambulance.org/" WHERE shortname="HAME";
+UPDATE agencies SET longname="Rochester Airport Fire", websiteurl="http://iaff1636.com/" WHERE shortname="AIRF";
+UPDATE agencies SET longname="Greece Ambulance", websiteurl="http://www.greeceambulance.com/" WHERE shortname="GREE";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="LAKF";
+UPDATE agencies SET longname="Rochester Institute of Technology Ambulance", websiteurl="http://ambulance.rit.edu/" WHERE shortname="RITE";
+
+UPDATE agencies SET longname="West Webster Ambulance", websiteurl="http://westwebsterfd.org/" WHERE shortname="WWBE";
+
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="WLKF";
+
+UPDATE agencies SET longname="Mumford Fire", websiteurl="http://www.mumfordfd.org/" WHERE shortname="MUMF";
+
+UPDATE agencies SET longname="Irondequoit Ambulance", websiteurl="http://www.irondequoitambulance.com/" WHERE shortname="IROE";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="MORF";
+UPDATE agencies SET longname="Perinton Ambulance", websiteurl="http://www.perintonambulance.org/" WHERE shortname="PERE";
+
+UPDATE agencies SET longname="Honeoye Falls Ambulance", websiteurl="http://www.honeoyefallsfd.com/" WHERE shortname="HFLF";
+
+UPDATE agencies SET longname="Scottsville Fire", websiteurl="http://www.scottsvillefd.org/" WHERE shortname="SCOF";
+
+UPDATE agencies SET longname="Clifton Fire", websiteurl="http://www.cliftonfire.org/index.php" WHERE shortname="CLIF";
+
+UPDATE agencies SET longname="Rush Ambulance", websiteurl="http://www.rushfd.org/" WHERE shortname="RUSE";
+UPDATE agencies SET longname="Penfield Ambulance", websiteurl="http://www.penfieldambulance.org/" WHERE shortname="PENE";
+
+UPDATE agencies SET longname="Honeoye Falls Fire", websiteurl="http://www.honeoyefallsfd.com/" WHERE shortname="HFLE";
+
+UPDATE agencies SET longname="East Rochester Ambulance", websiteurl="http://www.erambulance.org/" WHERE shortname="EROE";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="OTHP";
+UPDATE agencies SET longname="Union Hill Association Ambulance", websiteurl="http://www.uhfd.org" WHERE shortname="UHIE";
+UPDATE agencies SET longname="Hilton Ambulance", websiteurl="http://www.hiltonfd.org/" WHERE shortname="HILE";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="SBRF";
+
+UPDATE agencies SET longname="Monroe County Fire Bureau", websiteurl="http://www.monroecounty.gov/safety-fire.php" WHERE shortname="MCFB";
+
+UPDATE agencies SET longname="Brockport Ambulance", websiteurl="http://www.brockportambulance.org/" WHERE shortname="BROE";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="OTHE";
+UPDATE agencies SET longname="Scottsville Ambulance", websiteurl="http://www.scottsvillefd.org/" WHERE shortname="SCOE";
+UPDATE agencies SET longname="", websiteurl="" WHERE shortname="BERE";
