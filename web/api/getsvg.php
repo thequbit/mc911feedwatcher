977ed2a8ec0758ac581f01bb7ad35556e7e44eea
thequbit <thequbit@gmail.com>
2013-04-05 03:26:12 -0400

    php api that calls python scripts to call php api to create SVG files .....

---
+++ web/api/getsvg.php
@@ -0,0 +1,62 @@
+<?php
+
+	require_once("../tools/UtilityManager.class.php");
+
+	// get passed in params
+	$type = $_GET['type'];
+	$date = $_GET['date'];
+
+	// do some sanity checking
+	$util = new UtilityManager();
+	if( $date == "" || $util->IsValidDate($date) == 0 || $util->IsValidDate($date) == False )
+	{
+		// not a valid date, set to today's date
+		$date = date("Y-m-d");
+	}
+
+	// deturmine what API we are returning
+	switch($type)
+	{
+		//
+		// Daily Incident Counts API Call
+		//
+		/*
+		case "dailycounts":
+			// from passed in $_GET[] ...
+			if( $date == "" )
+				$date = date("Y-m-d");
+		
+			$results = $mgr->GetCountsByDay($date);
+			break;
+		*/
+		
+		//
+		// Incident Specific API Calls
+		//
+		case "barkingdogs":
+			$scriptName = "barkingdogs.py";
+			break;
+	
+		case "mva":
+			$scriptName = "mva.py";
+			break;
+	
+		//
+		// All time summation of all incidents
+		//
+		case "alltimesum":
+			$scriptName = "alltimesum.py";
+			break;
+			
+		//
+		// I no API good ...
+		//
+		default:
+			echo "Invalid Input.";
+			break;
+	}
+
+	exec("python " .  $scriptName, $svgImage);
+	echo $svgImage[1];
+
+?>
