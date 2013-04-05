870c29ebaf540b14498be8205a99520214ff8fe8
thequbit <thequbit@gmail.com>
2013-04-05 02:00:25 -0400

    migrated over to prepared statements

--- web/tools/IncidentManager.class.php
+++ web/tools/IncidentManager.class.php
@@ -1,5 +1,6 @@
 <?php
 
+	require_once("DatabaseManager2.class.php");
 	require_once("DatabaseManager.class.php");
 	require_once("Incident.class.php");
 	
@@ -15,229 +16,252 @@
 	
 		function GetIncidentsByDay($date)
 		{
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
+			dprint( "GetIncidentsByDay() Start." );
 			
-			// sanitize inputs
-			$date = $db->SanitizeInput($date);
-			
-			// create the query
-			$query = 'SELECT DISTINCT itemid,event,address,pubdate,pubtime,status,itemid,scrapedatetime,agencyid FROM incidents WHERE pubdate="' . $date . '" GROUP BY itemid ORDER BY pubtime DESC';
-			
-			// execute the query
-			$results = $db->Query($query);
-			
-			// create an array to put our results into
-			$incidents = array();
-			
-			// decode the rows
-			while($r = mysql_fetch_assoc($results)) {
-			
-				$incident = new Incident();
-			
-				// pull the information from the row
-				$incident->event = $r['event'];
-				$incident->address = $r['address'];
-				$incident->pubdate = $r['pubdate'];
-				$incident->pubtime = $r['pubtime'];
-				$incident->status = $r['status'];
-				$incident->itemid = $r['itemid'];
-				$incident->scrapedatetime = $r['scrapedatetime'];
-				$incident->agencyid = $r['agencyid'];
-				
-				$incidents[] = $incident;
-			}
-			
-			return $incidents;
-		}
+			try
+			{
 		
-		function GetIncidentsByAgencyShortName($agencyshortname, $count)
-		{
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
+				$db = new DatabaseTool();
 			
-			// sanitize inputs
-			$agencyshortname = $db->SanitizeInput($agencyshortname);
-			$count = $db->SanitizeInput($count);
+				if( $date == "" )
+					$date = date("Y-m-d");
 			
-			// create the query
-			$query = 'SELECT DISTINCT itemid,event,address,pubdate,pubtime,status,itemid,scrapedatetime FROM incidents WHERE itemid LIKE "%' . $agencyshortname . '%" GROUP BY itemid ORDER BY pubdate DESC LIMIT ' . $count;
+				// create the query
+				$query = 'SELECT DISTINCT itemid,event,address,pubdate,pubtime,status,itemid,scrapedatetime,agencyid FROM incidents WHERE pubdate = ? GROUP BY itemid ORDER BY pubtime DESC';
 			
-			// execute the query
-			$results = $db->Query($query);
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("s", $date); // bind the varibale
+				$results = $db->Execute($stmt);
 			
-			// create an array to put our results into
-			$incidents = array();
-			
-			// decode the rows
-			while($r = mysql_fetch_assoc($results)) {
-			
-				$incident = new Incident();
-			
-				// pull the information from the row
-				$incident->event = $r['event'];
-				$incident->address = $r['address'];
-				$incident->pubdate = $r['pubdate'];
-				$incident->pubtime = $r['pubtime'];
-				$incident->status = $r['status'];
-				$incident->itemid = $r['itemid'];
-				$incident->scrapedatetime = $r['scrapedatetime'];
+				// create an array to put our results into
+				$incidents = array();
 				
-				$incidents[] = $incident;
+				// decode the rows
+				foreach( $results as $result )
+				{
+					$incident = new Incident();
+				
+					// pull the information from the row
+					$incident->event = $result['event'];
+					$incident->address = $result['address'];
+					$incident->pubdate = $result['pubdate'];
+					$incident->pubtime = $result['pubtime'];
+					$incident->status = $result['status'];
+					$incident->itemid = $result['itemid'];
+					$incident->scrapedatetime = $result['scrapedatetime'];
+					$incident->agencyid = $result['agencyid'];
+					
+					$incidents[] = $incident;
+				}
+			
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
+			
 			}
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
+			}
+			
+			dprint("GetIncidentsByDay() Done.");
 			
 			return $incidents;
 		}
 		
 		function GetIncidentsByAgencyID($agencyid, $count)
 		{
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
+			dprint( "GetIncidentsByAgencyID() Start." );
 			
-			// sanitize inputs
-			$agencyid = $db->SanitizeInput($agencyid);
-			$count = $db->SanitizeInput($count);
-			
-			//
-			// note: I don't like how we are doing ORDER BY here, but we don't have enough information to do it differently.
-			// TODO: Look into better ways to do this.
-			//
-			
-			// create the query
-			$query = 'SELECT DISTINCT itemid,event,address,pubdate,pubtime,status,itemid,scrapedatetime FROM incidents WHERE agencyid = ' . $agencyid . ' GROUP BY itemid ORDER BY pubdate DESC LIMIT ' . $count;
-			
-			// execute the query
-			$results = $db->Query($query);
-			
-			$incidents = array();
-			
-			// decode the rows
-			while($r = mysql_fetch_assoc($results)) {
-			
-				$incident = new Incident();
-			
-				// pull the information from the row
-				$incident->event = $r['event'];
-				$incident->address = $r['address'];
-				$incident->pubdate = $r['pubdate'];
-				$incident->pubtime = $r['pubtime'];
-				$incident->status = $r['status'];
-				$incident->itemid = $r['itemid'];
-				$incident->scrapedatetime = $r['scrapedatetime'];
+			try
+			{
+		
+				$db = new DatabaseTool();
 				
-				$incidents[] = $incident;
+				if( $count == "" || $count == 0 )
+					$count = 25; // default value
+			
+				// create the query
+				$query = 'SELECT DISTINCT itemid,event,address,pubdate,pubtime,status,itemid,scrapedatetime FROM incidents WHERE agencyid = ? GROUP BY itemid ORDER BY pubdate DESC LIMIT ?';
+			
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("ii", $agencyid, $count); // bind the variable
+				$results = $db->Execute($stmt);
+			
+				// create an array to put our results into
+				$incidents = array();
+				
+				// decode the rows
+				foreach( $results as $result )
+				{
+					$incident = new Incident();
+				
+					// pull the information from the row
+					$incident->event = $result['event'];
+					$incident->address = $result['address'];
+					$incident->pubdate = $result['pubdate'];
+					$incident->pubtime = $result['pubtime'];
+					$incident->status = $result['status'];
+					$incident->itemid = $result['itemid'];
+					$incident->scrapedatetime = $result['scrapedatetime'];
+					$incident->agencyid = $result['agencyid'];
+					
+					$incidents[] = $incident;
+				}
+			
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
+			
 			}
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
+			}
+			
+			dprint("GetIncidentsByAgencyID() Done.");
 			
 			return $incidents;
 		}
 		
 		function GetYearIncidentsByAgencyID($agencyID)
 		{
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
+			dprint( "GetYearIncidentsByAgencyID() Start." );
 			
-			// sanitize inputs
-			$agencyid = $db->SanitizeInput($agencyid);
+			try
+			{
+		
+				$db = new DatabaseTool();
+				
+				$date = date("Y") . "-01-01";
 			
-			// create the query
-			$query = 'SELECT COUNT(DISTINCT itemid)  FROM incidents WHERE agencyid = ' . $agencyID . ' AND pubdate >= "' . date("Y") . '-01-01"';
+				// create the query
+				$query = 'SELECT COUNT(DISTINCT itemid) as count FROM incidents WHERE agencyid = ? AND pubdate >= ?';
 			
-			// execute the query
-			$results = $db->Query($query);
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("is", $agencyid, $date); // bind the variable
+				$results = $db->Execute($stmt);
 			
-			// get the row
-			$r = mysql_fetch_assoc($results);
+				// get the count
+				$count = $results[0]['count'];
 			
-			// get the count
-			$count = $r['COUNT(DISTINCT itemid)'];
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
 			
-			//echo $count;
+			}
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
+			}
 			
-			// return our created object
+			dprint("GetYearIncidentsByAgencyID() Done.");
+			
 			return $count;
 		}
 		
 		function GetIncidentCountByAgencyIDAndDate($agencyid, $date)
 		{
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
+			dprint( "GetIncidentCountByAgencyIDAndDate() Start." );
 			
-			// sanitize inputs
-			$agencyid = $db->SanitizeInput($agencyid);
-			$date = $db->SanitizeInput($date);
+			try
+			{
+		
+				$db = new DatabaseTool();
+				
+				if( $date == "" )
+					$date = date("Y-m-d");
 			
-			// create the query
-			$query = 'SELECT COUNT(DISTINCT itemid) FROM incidents WHERE agencyid = ' . $agencyid . ' AND pubdate = "' . $date . '"';
-
-			// execute the query
-			$results = $db->Query($query);
+				// create the query
+				$query = 'SELECT COUNT(DISTINCT itemid) as count FROM incidents WHERE agencyid = ? AND pubdate = ?';
 			
-			// get the row
-			$r = mysql_fetch_assoc($results);
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("is", $agencyid, $date); // bind the variable
+				$results = $db->Execute($stmt);
 			
-			// get the count
-			$count = $r['COUNT(DISTINCT itemid)'];
+				// get the count
+				$count = $results[0]['count'];
 			
-			//echo $count;
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
 			
-			// return our created object
+			}
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
+			}
+			
+			dprint("GetIncidentCountByAgencyIDAndDate() Done.");
+			
 			return $count;
 		}
 		
 		function GetIncidentCountsByDate($date)
 		{
 			
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
-		
-			// sanitize inputs
-			$date = $db->SanitizeInput($date);
-		
-			// get the counts for all incidents seen today
-			$query = 'select count(distinct itemid) as count, event from incidents where pubdate="' . $date . '" group by event';
+			dprint( "GetIncidentCountsByDate() Start." );
 			
-			// execute the query
-			$results = $db->Query($query);
-		
-			// create a dictionary to hold our results in
-			$dict = array();
-		
-			// generate dictionary of results
-			while($r = mysql_fetch_assoc($results)) {
-			
-				// decode the row data
-				$event = strtolower($r['event']);
-				$count = $r['count'];
-				
-				// add the key and value to the dictionary
-				$dict[$event] = $count;
-				
-			}
-		
-			// create an instance of our event manager
-			$eventManager = new EventManager();
-			
-			// get all of the known event types
-			$eventtypes = $eventManager->GetEventTypes();
-			
-			// create an array to return that has our counts in it
-			$counts = array();
-			
-			// create our count list based on our dictionary entries
-			foreach($eventtypes as $eventtype)
+			try
 			{
-				$id = strtolower($eventtype->eventtype);
+		
+				$db = new DatabaseTool();
+		
+				if( $date == "" )
+					$date = date("Y-m-d");
 			
-				// add the count for the event type to the counts array.  If it doesn't exist, it will be zero
-				$counts[] = $dict[$id];
+				// get the counts for all incidents seen today
+				$query = 'select count(distinct itemid) as count, event from incidents where pubdate = ? group by event';
+			
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("s", $date); // bind the variable
+				$results = $db->Execute($stmt);
+		
+				// create a dictionary to hold our results in
+				$dict = array();
+		
+				foreach( $results as $result )
+				{
+			
+					// decode the row data
+					$event = strtolower($result['event']);
+					$count = $result['count'];
+					
+					// add the key and value to the dictionary
+					$dict[$event] = $count;
+				
+				}
+		
+				// create an instance of our event manager
+				$eventManager = new EventManager();
+				
+				// get all of the known event types
+				$eventtypes = $eventManager->GetEventTypes();
+				
+				// create an array to return that has our counts in it
+				$counts = array();
+				
+				// create our count list based on our dictionary entries
+				foreach($eventtypes as $eventtype)
+				{
+					$id = strtolower($eventtype->eventtype);
+				
+					// add the count for the event type to the counts array.  If it doesn't exist, it will be zero
+					$counts[] = $dict[$id];
+				}
+				
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
+			
+			}
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
 			}
 			
+			dprint("GetIncidentCountsByDate() Done.");
+				
 			// return our array of counts for the event types
 			return $counts;
 			
