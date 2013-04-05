43de524687604b19db99ccecb85a15505b2ab89c
thequbit <thequbit@gmail.com>
2013-04-05 00:29:30 -0400

    added sanity checking for data param

--- web/api/counts.php
+++ web/api/counts.php
@@ -1,21 +1,31 @@
 <?php
 
+	require_once("../tools/UtilityManager.class.php");
 	require_once("../tools/APIManager.class.php");
 	
+	// get passed in params
+	$type = $_GET['type'];
+	$date = $_GET['date'];
+	
+	// do some sanity checking
+	$util = new UtilityManager();
+	if( $date == "" || $util->IsValidDate($date) == 0 || $util->IsValidDate($date) == False )
+	{
+		// not a valid date, set to today's date
+		$date = date("Y-m-d");
+	}
+	
+	// create an instance of our manager
 	$mgr = new APIManager();
 	
-	$type = $_GET['type'];
-	
-	//echo $type;
-	
+	// deturmine what API we are returning
 	switch($type)
 	{
 		//
 		// Daily Incident Counts API Call
 		//
 		case "dailycounts":
-			$date = $_GET["date"];
-		
+			// from passed in $_GET[] ...
 			if( $date == "" )
 				$date = date("Y-m-d");
 		
@@ -32,7 +42,6 @@
 		case "mva":
 			$results = $mgr->GetMVACounts();
 			break;
-	
 	
 		//
 		// All time summation of all incidents
@@ -51,10 +60,8 @@
 			break;
 	}
 	
-	//$results = $mgr->GetAllCounts();
-	
+	// encode and return
 	$jsonResults = json_encode($results);
-	
 	echo $jsonResults;
 
 ?>
