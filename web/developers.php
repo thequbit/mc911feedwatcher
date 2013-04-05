8d51ad7d3d6e72aa3bdee977844df4eb5c6d11e0
thequbit <thequbit@gmail.com>
2013-04-05 02:15:28 -0400

    updated developers page and added link to _header.php

--- web/developers.php
+++ web/developers.php
@@ -9,11 +9,27 @@
 				<br>
 				<br>
 				
-				Developer information comming soon.  For now checkout the GitHub repo <a href="https://github.com/thequbit/mc911feedwatcher">here</a>.  It has a lot of information on how to use the API's on the site.
-			
+				All code for the site as well as the scrapers can be found in the GitHub repo <a href="https://github.com/thequbit/mc911feedwatcher">here</a>.<br>
 				<br>
+				
+				For information on how to use the JSON API's, check out the GitHub WiKi <a href="https://github.com/thequbit/mc911feedwatcher/wiki">here</a>.  It includes definitions of the API calls that return JSON objects, as well as example Python code that uses PyGal to render SVG images with the data the API's return.<br>
 				<br>
-			
+				
+				<h4>Example API JSON Objects</h4>
+				
+				<div class="tab">
+					<a href="http://mycodespace.net/projects/mcsafetyfeed/api/counts.php?type=mva">Daily Counts of Motor Vehicle Accodents</a><br>
+					<a href="http://mycodespace.net/projects/mcsafetyfeed/api/counts.php?type=barkingdogs">Daily Counts of Backing Dog Complaints</a><br>
+					<a href="http://mycodespace.net/projects/mcsafetyfeed/api/counts.php?type=dailycounts">Today's COunts for all Incident Types</a><br>
+				</div>
+				<br>
+				
+				If you would like additional API's added, please use the contact information found on the <a href="about.php">about</a> page.<br>
+				<br>
+				
+				Note: API access is not currently restricted since the demand is not high.  If the demand increases, there may be API limitations put in place.  If you would like a data drop of the data just contact me and I will be happy to shoot it over to you :D.
+				<br>
+
 <?php
 	require_once("_footer.php");
 ?>
