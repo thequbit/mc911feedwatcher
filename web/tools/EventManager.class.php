fc2bbd066fc097dde943a7d37f576b79a7f098ce
thequbit <thequbit@gmail.com>
2013-04-05 01:42:24 -0400

    migrated over to prepared statements

--- web/tools/EventManager.class.php
+++ web/tools/EventManager.class.php
@@ -1,5 +1,6 @@
 <?php
 
+	require_once("DatabaseManager2.class.php");
 	require_once("DatabaseManager.class.php");
 	require_once("Event.class.php");
 	require_once("EventType.class.php");
@@ -9,183 +10,245 @@
 	
 		function GetEventTypes()
 		{
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
+			dprint( "GetEventTypes() Start." );
 			
-			// create the query
-			$query = 'SELECT * FROM eventtypes';
+			try
+			{
+		
+				$db = new DatabaseTool();
 			
-			// execute the query
-			$results = $db->Query($query);
+				// create the query
+				$query = 'SELECT eventtypeid,eventtype FROM eventtypes';
 			
-			// create an array to put our objects into
-			$eventtypes = array();
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$results = $db->Execute($stmt);
 			
-			// decode the rows
-			while($r = mysql_fetch_assoc($results)) {
+				dprint( "Processing " . count($results) . " Results ..." );
 			
-				// create a new event object
-				$eventtype = new EventType();
+				// create an array to put our objects into
+				$eventtypes = array();
 			
-				// pull the information from the row
-				$eventtype->eventtypeid = $r['eventtypeid'];
-				$eventtype->eventtype = $r['eventtype'];
+				// decode the rows
+				foreach( $results as $result )
+				{
+					// create a new event object
+					$eventtype = new EventType();
 				
-				// add out event type to the array
-				$eventtypes[] = $eventtype;
+					// pull the information from the row
+					$eventtype->eventtypeid = $result['eventtypeid'];
+					$eventtype->eventtype = $result['eventtype'];
+					
+					// add out event type to the array
+					$eventtypes[] = $eventtype;
+				}
+			
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
+			
+			}
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
 			}
 			
-			// return the array of event types
+			dprint("GetEventTypes() Done.");
+			
 			return $eventtypes;
 		}
 		
 		function GetEventTextFromID($id)
 		{
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
+			dprint( "GetEventTextFromID() Start." );
+			
+			try
+			{
 		
-			// sanitize inputs
-			$id = $db->SanitizeInput($id);
+				$db = new DatabaseTool();
 	
-			// create the query
-			$query = 'SELECT eventtype FROM eventtypes WHERE eventtypeid=' . $id;
+				// create the query
+				$query = 'SELECT eventtype FROM eventtypes WHERE eventtypeid= ?';
 			
-			// execute the query
-			$results = $db->Query($query);
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("s", $id); // bind the varibale
+				$results = $db->Execute($stmt);
+				
+				dprint( "Processing " . count($results) . " Results ..." );
+				
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
 			
-			// get the row
-			$r = mysql_fetch_assoc($results);
+				$eventtype = $results[0]["eventtype"];
 			
-			// return the count
-			return $r["eventtype"];
+			}
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
+			}
+			
+			dprint("GetEventTextFromID() Done.");
+		
+			return $eventtype;
 		}
 		
 		function GetAllTimeHourlyCountsByEventId($eventtypeid)
 		{
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
-
-			// sanitize inputs
-			$eventtypeid = $db->SanitizeInput($eventtypeid);
-
-			// get the eventtype via the id
-			$eventtype = $this->GetEventTextFromID($eventtypeid);
-
-			// generate query
-			$query = 'SELECT COUNT(DISTINCT itemid), pubtime FROM incidents WHERE LOWER(event)="' . $eventtype . '" GROUP BY HOUR(pubtime)';
+			dprint( "GetAllTimeHourlyCountsByEventId() Start." );
 			
-			// execute the query
-			$results = $db->Query($query);
-			
-			// create an array to place the events into
-			$events = array();
-			
-			// populate the array of event counts
-			while($r = mysql_fetch_assoc($results)) {
-				$event = new Event();
-				
-				$event->pubtime = $r['pubtime'];
-				$event->count = $r['COUNT(DISTINCT itemid)'];
-				
-				$events[] = $event;
-			}
-		
-			// create an array to put the counts into
-			$counts = array();
-			
-			// calculate the counts for each hour
-			for($hour=0; $hour<24; $hour++)
+			try
 			{
+		
+				$db = new DatabaseTool();
+
+				// get the eventtype via the id
+				$eventtype = $this->GetEventTextFromID($eventtypeid);
+
+				// generate query
+				$query = 'SELECT COUNT(DISTINCT itemid) as count, pubtime FROM incidents WHERE LOWER(event) = LOWER(?) GROUP BY HOUR(pubtime)';
 			
-				$count = 0;
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("s", $eventtype); // bind the varibale
+				$results = $db->Execute($stmt);
 			
-				// decode the rows
-				foreach($events as $event)
+				dprint( "Processing " . count($results) . " Results ..." );
+				
+				// create an array to place the events into
+				$events = array();
+			
+				// populate the array of event counts
+				foreach($results as $result)
+				{
+					$event = new Event();
+					
+					$event->pubtime = $result['pubtime'];
+					$event->count = $result['count'];
+					
+					$events[] = $event;
+				}
+		
+				// create an array to put the counts into
+				$counts = array();
+			
+				$hour = 0;
+			
+				// calculate the counts for each hour
+				for($hour=0; $hour<24; $hour++)
 				{
 				
-					// decode the hour of the incident
-					$pubhour = (int)substr($event->pubtime,0,2);
+					$count = 0;
 				
-					// if the current hour is the published hour, then add to the count
-					if( $pubhour == $hour )
+					// decode the rows
+					foreach($events as $event)
 					{
-						$count = $count + $event->count;
+					
+						// decode the hour of the incident
+						$pubhour = (int)substr($event->pubtime,0,2);
+					
+						// if the current hour is the published hour, then add to the count
+						if( $pubhour == $hour )
+						{
+							$count = $count + $event->count;
+						}
 					}
+				
+					// add the count to the array
+					$counts[] = $count;
+				
 				}
-			
-				// add the count to the array
-				$counts[] = $count;
+		
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
 			
 			}
-		
-			// return the count
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
+			}
+			
+			dprint("GetAllTimeHourlyCountsByEventId() Done.");
+
 			return $counts;
 		}
 		
 		function GetHourlyCountsByEventId($eventtypeid, $date)
 		{
-			// connect to the database
-			$db = new DatabaseManager();
-			$db->Connect();
-
-			// sanitize inputs
-			$eventtypeid = $db->SanitizeInput($eventtypeid);
-			$date = $db->SanitizeInput($date);
-
-			// get the eventtype via the id
-			$eventtype = $this->GetEventTextFromID($eventtypeid);
-
-			// genrate the query
-			$query = 'SELECT COUNT(DISTINCT itemid), pubtime FROM incidents WHERE LOWER(event)="' . $eventtype . '" AND pubdate="' . $date . '" GROUP BY HOUR(pubtime)';
+			dprint( "GetAllTimeHourlyCountsByEventId() Start." );
 			
-			// execute the query
-			$results = $db->Query($query);
-			
-			// create an array to place the events into
-			$events = array();
-			
-			// populate the array of event counts
-			while($r = mysql_fetch_assoc($results)) {
-				$event = new Event();
-				
-				$event->pubtime = $r['pubtime'];
-				$event->count = $r['COUNT(DISTINCT itemid)'];
-				
-				$events[] = $event;
-			}
-		
-			// create an array to put the counts into
-			$counts = array();
-			
-			// calculate the counts for each hour
-			for($hour=0; $hour<24; $hour++)
+			try
 			{
+		
+				$db = new DatabaseTool();
+
+				// get the eventtype via the id
+				$eventtype = $this->GetEventTextFromID($eventtypeid);
+				if( $date == "" )
+					$date = date("Y-m-d");
+
+				// genrate the query
+				$query = 'SELECT COUNT(DISTINCT itemid) as count, pubtime FROM incidents WHERE LOWER(event)="' . $eventtype . '" AND pubdate="' . $date . '" GROUP BY HOUR(pubtime)';
 			
-				$count = 0;
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("ss", $eventtype, $date); // bind the varibale
+				$results = $db->Execute($stmt);
 			
-				// decode the rows
-				foreach($events as $event)
+				dprint( "Processing " . count($results) . " Results ..." );
+			
+				// create an array to place the events into
+				$events = array();
+			
+				// populate the array of event counts
+				foreach($results as $result)
+				{
+					$event = new Event();
+					
+					$event->pubtime = $result['pubtime'];
+					$event->count = $result['count'];
+					
+					$events[] = $event;
+				}
+		
+				// create an array to put the counts into
+				$counts = array();
+				
+				// calculate the counts for each hour
+				for($hour=0; $hour<24; $hour++)
 				{
 				
-					// decode the hour of the incident
-					$pubhour = (int)substr($event->pubtime,0,2);
+					$count = 0;
 				
-					// if the current hour is the published hour, then add to the count
-					if( $pubhour == $hour )
+					// decode the rows
+					foreach($events as $event)
 					{
-						$count = $count + $event->count;
+					
+						// decode the hour of the incident
+						$pubhour = (int)substr($event->pubtime,0,2);
+					
+						// if the current hour is the published hour, then add to the count
+						if( $pubhour == $hour )
+						{
+							$count = $count + $event->count;
+						}
 					}
+				
+					// add the count to the array
+					$counts[] = $count;
+				
 				}
-			
-				// add the count to the array
-				$counts[] = $count;
+		
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
 			
 			}
-		
-			// return the count
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
+			}
+			
+			dprint("GetAllTimeHourlyCountsByEventId() Done.");
+
 			return $counts;
 		}
 	
