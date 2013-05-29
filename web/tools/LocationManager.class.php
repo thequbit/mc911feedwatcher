1c1e2ed3ca83ecf00e647fae8895226f487ecc38
thequbit <thequbit@gmail.com>
2013-05-29 16:18:08 -0400

    added ByDateByType to location manager

--- web/tools/LocationManager.class.php
+++ web/tools/LocationManager.class.php
@@ -18,7 +18,7 @@
 					$date = date("Y-m-d");
 			
 				// create the query
-				$query = 'SELECT DISTINCT itemid,event,fulladdress,lat,lng FROM incidents WHERE pubdate = ? and lat <> "" and lng <> "" GROUP BY itemid ORDER BY pubtime DESC';
+				$query = 'SELECT DISTINCT itemid,event,fulladdress,lat,lng FROM incidents WHERE pubdate = ? AND lat <> "" AND lng <> "" GROUP BY itemid ORDER BY pubtime DESC';
 			
 				$mysqli = $db->Connect();
 				$stmt = $mysqli->prepare($query);
@@ -52,6 +52,57 @@
 			}
 			
 			dprint("GetLocationsByDay() Done.");
+			
+			return $incidents;
+		}
+		
+		function GetLocationsByDayByType($date,$type)
+		{
+			dprint( "GetLocationsByDayByType() Start." );
+			
+			try
+			{
+		
+				$db = new DatabaseTool();
+			
+				if( $date == "" )
+					$date = date("Y-m-d");
+			
+				// create the query
+				$query = 'SELECT DISTINCT itemid,event,fulladdress,lat,lng FROM incidents JOIN eventtypes ON incidents.event = eventtypes.eventtype WHERE pubdate = ? AND lat <> "" AND lng <> "" AND eventtypes.eventtypeid = ? GROUP BY itemid ORDER BY pubtime DESC;';
+			
+				$mysqli = $db->Connect();
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("ss", $date,$type); // bind the varibale
+				$results = $db->Execute($stmt);
+			
+				// create an array to put our results into
+				$incidents = array();
+				
+				// decode the rows
+				foreach( $results as $result )
+				{
+					$incident = (object) array(
+													//'itemid' => $result['itemid'],
+													'event' => $result['event'],
+													//'fulladdress' => $result['fulladdress'],
+													'lat' => $result['lat'],
+													'lng' => $result['lng']
+												  );
+					
+					$incidents[] = $incident;
+				}
+			
+				// close our DB connection
+				$db->Close($mysqli, $stmt);
+			
+			}
+			catch (Exception $e)
+			{
+				dprint( "Caught exception: " . $e->getMessage() );
+			}
+			
+			dprint("GetLocationsByDayByType() Done.");
 			
 			return $incidents;
 		}
