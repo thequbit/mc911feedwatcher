0fab231adaa24941a1d73c25abe152f10d88886c
thequbit <thequbit@gmail.com>
2013-06-05 02:55:42 -0400

    cleaned up example code

--- web/geotest.php
+++ web/geotest.php
@@ -12,44 +12,44 @@
 	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 	<script type="text/javascript">
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: new google.maps.LatLng(43.1547, -77.6158),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 10,
+			center: new google.maps.LatLng(43.1547, -77.6158),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
 
-	loadData();
-		
-    function loadData()
-    {
-        var url = "http://mcsafetyfeed.org/api/getgeo.php";
-        $.getJSON(url, function (response) {handleData(response)});
-    }
+		loadData();
+			
+		function loadData()
+		{
+			var url = "http://mcsafetyfeed.org/api/getgeo.php";
+			$.getJSON(url, function (response) {handleData(response)});
+		}
 
-    function handleData(response)
-    {
-        var n;
-        for(n=0; n<response.length; n++)
-        {
-            //name = response.drivers[n].name;
-            //alert(name);
-            lat = response[n].lat;
-            lng = response[n].lng;
-			event = response[n].event;
-            var myLatLng = new google.maps.LatLng(lat,lng);
-            var marker = new google.maps.Marker({
-                position: myLatLng,
-                //shadow: shadow,
-                //icon:image,
-                map: map,
-                title: event,
-                zIndex: 1
-            });
-        }   
-    }
+		function handleData(response)
+		{
+			var n;
+			for(n=0; n<response.length; n++)
+			{
+				//name = response.drivers[n].name;
+				//alert(name);
+				lat = response[n].lat;
+				lng = response[n].lng;
+				event = response[n].event;
+				var myLatLng = new google.maps.LatLng(lat,lng);
+				var marker = new google.maps.Marker({
+					position: myLatLng,
+					//shadow: shadow,
+					//icon:image,
+					map: map,
+					title: event,
+					zIndex: 1
+				});
+			}   
+		}
 
 
 
-  </script>
+	</script>
 </body>
 </html>
