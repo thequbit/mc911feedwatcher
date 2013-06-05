6a28548f3facb2ff1587abcf601412e06eba9dfa
thequbit <thequbit@gmail.com>
2013-06-05 02:31:41 -0400

    updated developers page toi include geo location api's

--- web/developers.php
+++ web/developers.php
@@ -15,15 +15,24 @@
 				For information on how to use the JSON API's, check out the GitHub WiKi <a href="https://github.com/thequbit/mc911feedwatcher/wiki">here</a>.  It includes definitions of the API calls that return JSON objects, as well as example Python code that uses PyGal to render SVG images with the data the API's return.<br>
 				<br>
 				
-				<h4>Example API JSON Objects</h4>
+				<h3>Example API JSON Objects</h3>
+				</br>
 				
 				<div class="tab">
-					<a href="http://mycodespace.net/projects/mcsafetyfeed/api/counts.php?type=mva">Daily Counts of Motor Vehicle Accodents</a><br>
-					<a href="http://mycodespace.net/projects/mcsafetyfeed/api/counts.php?type=barkingdogs">Daily Counts of Backing Dog Complaints</a><br>
-					<a href="http://mycodespace.net/projects/mcsafetyfeed/api/counts.php?type=dailycounts">Today's Counts for all Incident Types</a><br>
-					<a href="http://mycodespace.net/projects/mcsafetyfeed/api/counts.php?type=alltimesum">All-Time Summation by Incident Type</a><br>
+					<h4>Incident Count API's</h4>
+					<a href="api/counts.php?type=mva">Daily Counts of Motor Vehicle Accodents</a><br>
+					<a href="api/counts.php?type=barkingdogs">Daily Counts of Backing Dog Complaints</a><br>
+					<a href="api/counts.php?type=dailycounts">Today's Counts for all Incident Types</a><br>
+					<a href="api/counts.php?type=alltimesum">All-Time Summation by Incident Type</a><br>
 				</div>
-				<br>
+				</br>
+				
+				<div class="tab">
+					<h4>Geo Location API's</h4>
+					<a href="api/getgeo.php">All of Today's incidents with Geo Location data</a><br>
+					<a href="api/getgeo.php?typeid=10">All of Today's 'human life endangered by animal' incidents with Geo Location data</a><br>
+				</div>
+				</br>
 				
 				If you would like additional API's added, please use the contact information found on the <a href="about.php">about</a> page.<br>
 				<br>
